15954e1b04b8f59b7bee08265d57cce886f2ff58
Donatas Martinkus <donatas.martinkus@hmcts.net>
2017-11-01 08:58:13 +0000

    Move javascript to file

---
+++ docs/network.js
@@ -0,0 +1,110 @@
+function build(data) {
+    // populate list
+    var apiList = document.getElementById('apis');
+
+    data.forEach(function(el) {
+        console.log(el);
+        if (el.spec) {
+            apiList.insertAdjacentHTML('beforeend', '<li><a href="./swagger.html?url=' + el.spec + '">' + el.name + '</a></li>');
+        } else {
+            apiList.insertAdjacentHTML('beforeend', '<li>' + el.name + '</li>');
+        }
+    });
+
+    var nodeData = data.map(function(micro) {
+        var href = "";
+        if (micro.spec) {
+            href = "\"href\": \"./swagger.html?url=" + micro.spec + "\","
+        }
+        return JSON.parse("{" +
+            "\"id\": \"" + micro.name + "\"," +
+            "\"label\": \"" + micro.name + "\"," +
+            href +
+            "\"value\": " + (micro.name.length * 20) +
+        "}");
+    });
+
+    var edgesData = data.map(function(micro) {
+        var source = micro.name;
+        var dependencies = micro.dependencies || [];
+        var edge = dependencies.reduce(function(acc, item) {
+            return acc + (acc === "" ? "" : ",") + "{" +
+                "\"from\": \"" + source + "\"," +
+                "\"to\": \"" + item.name + "\"" +
+            "}";
+        }, "");
+
+        return JSON.parse('[' + edge + ']');
+    });
+    var edgesFlatData = [].concat.apply([], edgesData);
+
+    // Instantiate our network object.
+    var container = document.getElementById('api');
+    var data = {
+        nodes: nodeData,
+        edges: edgesFlatData
+    };
+    var options = {
+        nodes: {
+            shape: 'dot',
+        },
+        edges: {
+            arrows: {
+                to: true
+            }
+        },
+        interaction: {
+            hover: true
+        }
+    };
+    var network = new vis.Network(container, data, options);
+    var networkCanvas = container.getElementsByTagName('canvas')[0];
+
+    network.on('click', function (params) {
+        var nodeId = this.getNodeAt(params.pointer.DOM);
+        var href = this.body.nodes[nodeId].options.href;
+
+        if (href) {
+            window.open(href, '_blank');
+        }
+    });
+
+    function changeCursor(newCursorStyle){
+        networkCanvas.style.cursor = newCursorStyle;
+    }
+
+    network.on('hoverNode', function (params) {
+        var href = this.body.nodes[params.node].options.href;
+
+        if (href) {
+            changeCursor('pointer');
+        }
+    });
+    network.on('blurNode', function (params) {
+        changeCursor('default');
+    });
+}
+
+// load data
+function loadJSON(file, callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType('application/json');
+    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == '200') {
+        // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+        callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);  
+    }
+    
+
+function load() {
+    loadJSON('./microservices.json', function(response) {
+        var microservices = JSON.parse(response);
+        build(microservices);
+    });
+}
+
+load();
