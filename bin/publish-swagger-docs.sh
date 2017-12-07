0e920ea69ef6020e2ec4d54f1da4f6c4ffe7bcdc
Donatas Martinkus <donatas.martinkus@hmcts.net>
2017-12-07 12:55:52 +0000

    Fix shell script

--- bin/publish-swagger-docs.sh
+++ bin/publish-swagger-docs.sh
@@ -3,6 +3,7 @@
 # central reform-api-docs repo. Updates reform-api-docs spec if needed
 
 # assign environment variables
+# shellcheck disable=SC1091
 . ./.env
 
 ./gradlew clean installDist
@@ -11,16 +12,16 @@ docker-compose up -d
 
 sleep 15
 
-REPO_NAME=$(echo ${TRAVIS_REPO_SLUG} | cut -f2- -d/)
-CURRENT_DOCS=$(curl https://hmcts.github.io/reform-api-docs/specs/${REPO_NAME}.json)
-NEW_DOCS=$(curl http://localhost:${SERVER_PORT}/v2/api-docs)
+REPO_NAME=$(echo "$TRAVIS_REPO_SLUG" | cut -f2- -d/)
+CURRENT_DOCS=$(curl https://hmcts.github.io/reform-api-docs/specs/"$REPO_NAME".json)
+NEW_DOCS=$(curl http://localhost:"$SERVER_PORT"/v2/api-docs)
 
 docker-compose stop
 
-if [ "$NEW_DOCS" == "" ]; then
+if [ "$NEW_DOCS" = "" ]; then
     echo "Could not retrieve new docs, aborting."
     docker-compose logs
-elif [[ "$CURRENT_DOCS" != "$NEW_DOCS" ]]; then
+elif [ "$CURRENT_DOCS" != "$NEW_DOCS" ]; then
     echo "Update reform-api-docs"
     mkdir swagger-staging
     cd swagger-staging
@@ -31,11 +32,11 @@ elif [[ "$CURRENT_DOCS" != "$NEW_DOCS" ]]; then
     git remote add upstream "https://${GH_TOKEN}@github.com/hmcts/reform-api-docs.git"
     git pull upstream master
 
-    TARGET_SPEC=docs/specs/${REPO_NAME}.json
-    echo ${NEW_DOCS} > ${TARGET_SPEC}
+    TARGET_SPEC=docs/specs/"$REPO_NAME".json
+    echo "$NEW_DOCS" > "$TARGET_SPEC"
 
-    git add ${TARGET_SPEC}
-    git commit -m "Updating spec for ${REPO_NAME} from ${TRAVIS_PULL_REQUEST_SLUG}"
+    git add "$TARGET_SPEC"
+    git commit -m "Updating spec for $REPO_NAME from $TRAVIS_PULL_REQUEST_SLUG"
     git push --set-upstream upstream master
 else
     echo "API Documentation is up to date."
