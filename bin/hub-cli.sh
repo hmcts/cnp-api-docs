51902e7d3d54e62fe9b85b8abd285c1d620a1d47
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-01-12 13:58:14 +0000

    Remove server name checkup and just downloading the hub

--- bin/hub-cli.sh
+++ bin/hub-cli.sh
@@ -1,27 +1,12 @@
 #!/usr/bin/env bash
 # Checks whether GitHub's CLI `hub` exists and installs one as needed
 
-if ! type "hub" > /dev/null; then
-    unameOut="$(uname -s)"
+mkdir ~/hub-latest
 
-    echo "Checking OS support for $unameOut"
+wget -O ~/hub-latest.tar.gz "$(curl https://api.github.com/repos/github/hub/releases/latest | jq -r '.assets | map(select(.label | test("for Linux 64-bit")))[0].browser_download_url')"
 
-    case "${unameOut}" in
-        Linux*)     machine="Linux 64-bit";;
-        Darwin*)    machine="OS X";;
-        *)          machine="UNKNOWN:$unameOut"
-    esac
+tar xf ~/hub-latest.tar.gz -C ~/hub-latest --strip-components 1
 
-    echo "Selected machine: $machine"
-
-    mkdir ~/hub-latest
-    wget -O ~/hub-latest.tar.gz "$(curl https://api.github.com/repos/github/hub/releases/latest | jq -r '.assets | map(select(.label | test("$machine")))[0].browser_download_url')"
-    tar xf ~/hub-latest.tar.gz -C ~/hub-latest --strip-components 1
-    ~/hub-latest/install
-
-    echo "Successfully installed hub:"
-else
-    echo "Hub is already installed!"
-fi
+~/hub-latest/install
 
 hub version
